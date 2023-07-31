b5063cf88b7eaf455b3a51727a094016e2a140f1
SMbrian <139880831+SMbrian@users.noreply.github.com>
2023-07-31 10:40:21 -0300

    Add log page and file middleware for protected routes

---
+++ src/middleware.ts
@@ -0,0 +1,19 @@
+import { NextRequest, NextResponse } from 'next/server';
+
+export function middleware(request: NextRequest) {
+  const currentUser = request.cookies.get('currentUser')?.value;
+
+  if (!currentUser) {
+    // Redirect to login page if user is not authenticated
+    const response = NextResponse.redirect(new URL('/login', request.url));
+    response.cookies.delete('currentUser');
+    return response;
+  }
+
+  // Allow request to proceed if user is authenticated
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ['', ], //add protected links
+};
